4e3bdbbc1c9bded5e8115b72eda8e6b4aef5b26d
Anastasiia Pohonets <29karasik@gmail.com>
2026-01-06 17:37:04 +0200

    Add authentication with JWT

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -146,4 +146,10 @@ REST_FRAMEWORK = {
         "anon": "100/day",
         "user": "1000/day"
     }
+}
+
+SIMPLE_JWT = {
+   "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+   "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+   "ROTATE_REFRESH_TOKENS": True
 }
